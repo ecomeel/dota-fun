597cba3d4d5dbaca59e8cd2c4b9e158278c86b1f
AleksandrPulin <ecomeel@gmail.com>
2024-08-23 17:59:49 +0300

    download bg steam image

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
   <main class="main container">
     <RouterView />
   </main>
+  <img 
+    alt=""
+    class="bg bg__1"
+    src="/steam-bg.png"
+  >
   <!-- Footer -->
 </template>
 
@@ -14,4 +19,13 @@ import TheHeader from '@/components/TheHeader.vue'
 .main {
   padding: 100px 0;
 }
+.bg {
+  position: absolute;
+  &__1 {
+    top: 100px;
+    left: 200px;
+    width: 70vw;
+  }
+}
+
 </style>
