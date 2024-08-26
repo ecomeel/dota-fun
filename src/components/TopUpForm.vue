77c87379a2386a240309af7d8f241a5916e1752c
AleksandrPulin <ecomeel@gmail.com>
2024-08-26 12:56:48 +0300

    adjust inputs

--- src/components/TopUpForm.vue
+++ src/components/TopUpForm.vue
@@ -2,16 +2,26 @@
   <ElForm class="topup" :model="form">
     <div class="topup__form">
       <ElFormItem label="Логин Steam">
-        <ElInput v-model="form.name" class="topup__input">
+        <ElInput
+          v-model="form.name"
+          class="topup__input" 
+          placeholder="Ваш логин"
+        >
           <template #append>
-            <!-- <div>Где взять логин <div>?</div></div> -->
-            где взять логин?
+            <div class="topup__input--tip">
+              <p>Где взять логин</p>
+              <div class="topup__input--tip-icon">?</div>
+            </div>
           </template>
         </ElInput>
       </ElFormItem>
       <ElFormItem label="Сумма">
-        <ElInput v-model="form.amount" type="number" />
-        <p>До 10 000 рублей</p>
+        <ElInput v-model="form.amount" type="number">
+          <template #append>
+            <p class="topup__input--tip">Комиссия сервиса 18%</p>
+          </template>
+        </ElInput>
+        <p class="topup">До 10 000 рублей</p>
       </ElFormItem>
       <ElFormItem>
         <ElSlider v-model="form.amount" />
@@ -28,12 +38,6 @@
 </template>
 
 <script setup lang="ts">
-// import {
-//   ElForm,
-//   ElFormItem,
-//   ElInput,
-//   ElSlider
-// } from 'element-plus'
 import { ref } from 'vue'
 
 const form = ref({
@@ -44,12 +48,59 @@ const form = ref({
 
 <style scoped lang="scss">
 .topup {
+  &__form > *:not(:first-child) {
+    margin-top: 20px;
+  }
   &__form {
     padding: 25px 20px;
     border: 1px solid #4E768F;
     border-radius: 15px;
     background-color: #1529368C;
   }
+  &__input {
+    &--tip {
+      position: absolute;
+      right: 10px;
+      top: calc(50% - 8px);
+      display: flex;
+      column-gap: 5px;
+      // font-size: 12px;
+      // color: #FFFFFFB2;
+      @extend %extra-small;
+
+      &-icon {
+        padding: 0 5px;
+        background-color: #D9D9D926;
+        border-radius: 100%;
+      }
+    }
+  }
 }
 </style>
 
+<style lang="scss">
+.el-form-item__label {
+  margin-bottom: 5px;
+}
+.el-input {
+  position: relative;
+  &__wrapper {
+
+  }
+  &__inner {
+    padding: 14px 0 14px 14px;
+    width: 100%;
+    border-radius: 15px;
+    border: none;
+    background-color: #26526F8C;
+    color: $white;
+    font-family: "Inter";
+    font-size: 20px;
+    line-height: 1.25em;
+
+    &::placeholder {
+      color: #FFFFFFB2;
+    }
+  }
+}
+</style>
