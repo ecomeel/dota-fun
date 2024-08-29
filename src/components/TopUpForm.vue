d126e0ee1320689f80ccd0b4c352b5c8c3dcb866
AleksandrPulin <ecomeel@gmail.com>
2024-08-29 15:20:10 +0300

    prefinish form layout

--- src/components/TopUpForm.vue
+++ src/components/TopUpForm.vue
@@ -7,32 +7,32 @@
           class="topup__input" 
           placeholder="Ваш логин"
         >
-          <!-- <template #append> -->
-            <!--
-              <div class="topup__input--tip">
+          <template #append>
+            <div class="topup__input--tip">
               <p>Где взять логин</p>
               <div class="topup__input--tip-icon">?</div>
-              </div> 
-            -->
-          <!-- </template> -->
+            </div> 
+          </template>
         </ElInput>
-        <div class="topup__input--tip">
-              <p>Где взять логин</p>
-              <div class="topup__input--tip-icon">?</div>
-            </div>
+        <!--
+          <div class="topup__input--tip">
+          <p>Где взять логин</p>
+          <div class="topup__input--tip-icon">?</div>
+          </div> 
+        -->
       </ElFormItem>
       <ElFormItem class="topup__item" label="Сумма">
-        <ElInput 
+        <ElInputNumber 
           v-model="form.amount"
+          :controls="false"
           :max="priceRange.max"
           :min="priceRange.min"
           placeholder="Введите сумму"
-          type="number"
         >
           <template #append>
             <p class="topup__input--tip">Комиссия сервиса 18%</p>
           </template>
-        </ElInput>
+        </ElInputNumber>
         <p class="topup__input--description">До 10 000 рублей</p>
       </ElFormItem>
       <ElFormItem>
@@ -46,22 +46,59 @@
         <p class="topup__input--description slider__description">Время зачисления до 60 секунд</p>
       </ElFormItem>
       <ElFormItem label="Почта">
-        <ElInput />
+        <ElInput v-model="form.email" placeholder="Для получения чека" />
       </ElFormItem>
     </div>
     <div class="topup__form">
-      <p>Hello</p>
+      <ElFormItem label="Промокод">
+        <ElInput v-model="form.promo" placeholder="Введите промокод">
+          <template #append>
+            <div class="topup__input--tip">
+              <p>Где взять промокод</p>
+              <div class="topup__input--tip-icon">?</div>
+            </div>
+          </template>
+        </ElInput>
+      </ElFormItem>
+      <ElFormItem label="Итого">
+        <div class="el-input">
+          <div class="el-input__wrapper" type="total">
+            <p class="el-input__inner">{{ totalSum }} рублей</p>
+          </div>
+        </div>
+      </ElFormItem>
+      <div class="topup__warning">
+        <p>Перед оплатой проверьте правильность указанного логина, иначе вы пополните стим другому человеку и мы не сможем вернуть деньги.</p>
+      </div>
+      <ElFormItem>
+        <ElCheckbox v-model="form.isAgree">Я принимаю условия Пользовательского соглашения
+          и подтверждаю ознакомление с FAQ</ElCheckbox>
+      </ElFormItem>
+      <button class="topup__button topup__field" @click.prevent="handleSendForm">Пополнить</button>
     </div>
   </ElForm>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
-const form = ref({
-  name: '',
+interface IForm {
+  name: string | null;
+  amount: number | null;
+  email: string | null;
+  promo: string | null;
+  isAgree: boolean
+}
+
+const form = ref<IForm>({
+  name: null,
   amount: null,
+  email: null,
+  promo: null,
+  isAgree: false
 })
+
+const totalSum = computed(() => Math.ceil(form.value.amount! * 1.18))
 
 const priceRange = {
   min: 100,
@@ -72,21 +109,36 @@ const priceRangeMarks = {
   [priceRange.min]: priceRange.min + ' ₽',
   [priceRange.max]: priceRange.max + ' ₽'
 }
+
+function handleSendForm () {
+  console.log('Send form')
+}
 </script>
 
 <style scoped lang="scss">
 .topup {
-  &__form > *:not(:first-child) {
-    margin-top: 20px;
-  }
   &__form {
     padding: 25px 20px;
+    flex: 0 0 48.7% !important;
     border: 1px solid #4E768F;
     border-radius: 15px;
     background-color: #1529368C;
+
+    & > *:not(:first-child) {
+      margin-top: 20px;
+    }
   }
   &__item {
     position: relative;
+  }
+  &__field {
+    padding: 14px 15px;
+    width: 100%;
+    border-radius: 15px;
+    font-size: inherit;
+    background-color: $blue-transparent;
+    color: $white;
+    border: none;
   }
   &__input {
     &--tip {
@@ -108,38 +160,36 @@ const priceRangeMarks = {
       @extend %extra-small;
     }
   }
+  &__warning {
+    padding: 11px 15px;
+    border-radius: 15px;
+    background-color: $blue-transparent;
+    p {
+      font-size: 15px;
+      line-height: 1.25em;
+      opacity: 0.7;
+    }
+  }
+  &__button {
+    margin-top: 17px !important;
+    cursor: pointer;
+    transition: background-color, border-color .2s linear;
+    border: 1px solid transparent;
+
+    &:hover {
+      background-color: #47718d8c;
+      border-color: #6d859c;
+    }
+    &:focus {
+      outline: none;
+      border-color: #6d859c;
+    }
+  }
 }
 
 .slider {
   &__description {
-    margin-top: 12px;
+    margin-top: 26px;
   }
 }
-</style>
-
-<style lang="scss">
-// .el-form-item__label {
-//   margin-bottom: 5px;
-// }
-// .el-input {
-//   position: relative;
-//   &__wrapper {
-
-//   }
-//   &__inner {
-//     padding: 14px 0 14px 14px;
-//     width: 100%;
-//     border-radius: 15px;
-//     border: none;
-//     // background-color: #26526F8C;
-//     color: $white;
-//     font-family: "Inter";
-//     font-size: 20px;
-//     line-height: 1.25em;
-
-//     &::placeholder {
-//       color: #FFFFFFB2;
-//     }
-//   }
-// }
 </style>
